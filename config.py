2907486f16e9df06d6f0ecb6920ce28828d80be6
liangyeni <liangyeni123@gmail.com>
2016-10-15 16:07:22 -0230

    microblog
    Using Python to create microblog which is similar to weibo.

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+OPENID_PROVIDERS = [
+    {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
+    {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
+    {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
+    {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
+    {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+WHOOSH_BASE = os.path.join(basedir, 'search.db')
+
+# email server
+MAIL_SERVER = 'smtp.mail.yahoo.com'
+MAIL_PORT = 465
+MAIL_USE_TLS = True
+MAIL_USE_SSL = True
+MAIL_USERNAME = os.environ.get('yeni.liang@yahoo.com')
+MAIL_PASSWORD = os.environ.get('zangma123')
+
+# administrator list
+ADMINS = ['yeni.liang@yahoo.com']
+
+# pagination
+POSTS_PER_PAGE = 3
+MAX_SEARCH_RESULTS = 50
+
